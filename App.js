277123e96a1868bf2dd19e9b4c52ee4907db6bf8
J <j.suomaki@gmail.com>
2022-03-17 16:34:48 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
+
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers]
+      })
+      setContacts(data);
+      console.log(data);
+      console.log("Nimi: "+data[0].name+" numero: "+data[0].phoneNumbers[0].number)
+    }
+  }
+
+  function TulostaNumero(props) {
+    const kontakti = props.kontakti;
+    let numero="no number";
+    try {
+      numero = kontakti.phoneNumbers[0].number;
+    } catch (error) {
+     //console.error(error);
+    }
+    console.log("Nimi:"+kontakti.name);
+    console.log("Numero:"+numero);
+    return (
+      <Text>
+        {numero}
+      </Text>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
       <StatusBar style="auto" />
+      {
+        hasPermission ? (
+          <View>
+            <Text>Contacts: {contacts.length}</Text>
+            <FlatList style={styles.lista}
+              data={contacts}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) =>
+            <Text>{item.firstName} <TulostaNumero kontakti={item}></TulostaNumero></Text>
+            }/>
+          </View>
+        ) : (
+            <Text>No permission to use Contacts</Text>
+          )
+      }
+      <Button title="Get contacts" onPress={getContacts} />
     </View>
   );
 }
@@ -16,5 +67,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 75
   },
+
 });
